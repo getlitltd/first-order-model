9eaa98cc02e324e3f0a1697e83973a81ab60198a
getlitltd <108588816+getlitltd@users.noreply.github.com>
2022-08-28 22:24:10 -0700

    Modified logger.py to use skimage.draw.disc instead of skimage.draw.circle. skimage.draw.circle has been deprecated.

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import imageio
 
 import os
-from skimage.draw import circle
+import skimage.draw.disk as circle
 
 import matplotlib.pyplot as plt
 import collections
